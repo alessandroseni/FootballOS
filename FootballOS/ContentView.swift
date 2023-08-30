448f9ff8780cc1447afe1203ac1a1b7fbd945967
Azeni <alessandroseni@gmail.com>
2023-08-30 11:59:33 -0500

    Initial Commit

---
+++ FootballOS/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  FootballOS
+//
+//  Created by Alessandro on 8/30/23.
+//
+
+import SwiftUI
+import RealityKit
+import RealityKitContent
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Model3D(named: "Scene", bundle: realityKitContentBundle)
+                .padding(.bottom, 50)
+
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview(windowStyle: .automatic) {
+    ContentView()
+}
---
+++ FootballOS/FootballOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  FootballOSApp.swift
+//  FootballOS
+//
+//  Created by Alessandro on 8/30/23.
+//
+
+import SwiftUI
+
+@main
+struct FootballOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
